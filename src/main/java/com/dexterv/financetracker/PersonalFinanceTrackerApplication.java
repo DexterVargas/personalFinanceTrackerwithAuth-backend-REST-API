d4113e599af4405ebbc46fbe3e63805905027704
dextervargasDev <dextervargas0912@gmail.com>
2025-10-03 09:24:24 +0800

    initial setup. added docker-compose.yml and resource props

--- src/main/java/com/dexterv/financetracker/PersonalFinanceTrackerApplication.java
+++ src/main/java/com/dexterv/financetracker/PersonalFinanceTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonalFinanceTrackerwithAuthApplication {
+public class PersonalFinanceTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PersonalFinanceTrackerwithAuthApplication.class, args);
+        SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
     }
 
 }
--- src/test/java/com/dexterv/financetracker/PersonalFinanceTrackerApplicationTests.java
+++ src/test/java/com/dexterv/financetracker/PersonalFinanceTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PersonalFinanceTrackerwithAuthApplicationTests {
+class PersonalFinanceTrackerApplicationTests {
 
     @Test
     void contextLoads() {
